0a22b6658fe52167203ed3d61525e63c236b11d7
Pasindu2000B <pasindupremasirilk@gmail.com>
2024-10-11 11:53:09 +0530

    First

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+
+
+
+    
+
+    
+    private static void Main(string[] args)
+    {
+        Program p = new Program();
+        int [][] arr=new int[2][];
+        arr[0]=[1,2,35];
+        arr[1]=[4,5,6];
+
+        int [][] jaggedArry=new int [2][]{
+            new int[]{1,2,3},
+            new int[]{4,5,6}
+        };
+       
+       
+    }
+}
